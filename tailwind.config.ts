282bc4b4b33cead4f242f1bf67071dff5ab568b5
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2024-01-10 01:07:43 +0100

    intial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        bgColor: "#FAFAFA",
+        secondary: "#1E2640",
+        primaryLightBlue: "#353C53",
+      }
     },
   },
   plugins: [],
